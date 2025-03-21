7e37ab6bbee40697aa88091ffbfeb2dc2eaad456
javarex <raymartnayreitanong11@gmail.com>
2025-03-21 21:48:28 +0800

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,6 +46,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->tenantProfile(EditTenantProfile::class)
             ->login(Login::class)
+            
             ->colors([
                 'primary' => Color::Blue,
                 'red' => Color::Red,
