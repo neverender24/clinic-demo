f4eba81193951d11af0326fddc355cdf79644e48
javarex <raymartnayreitanong11@email.com>
2025-03-26 20:02:37 +0800

    update

--- resources/views/consultations/admitting-order.blade.php
+++ resources/views/consultations/admitting-order.blade.php
@@ -80,7 +80,7 @@
                 </div>
                 <div class="col-span-3 physician-signature gap-y-0 text-[7pt] flex justify-end">
                    <div>
-                     <p>BEN JAY C. PORCADILLA, RMT, MD, FPCP</p>
+                     <p class="font-bold">BEN JAY C. PORCADILLA, RMT, MD, FPCP</p>
                      <p>License no: 0132066</p>
                      <p>PTR no: 2173419</p>
                      <p>S2 License no: _____________________</p>
