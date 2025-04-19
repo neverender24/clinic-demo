4ea092add4a6f60b01aa2b42b1290e665d2d69e2
javarex <raymartnayreitanong11@gmail.com>
2025-04-19 14:26:46 +0800

    update

--- app/Filament/Resources/HospitalAdmissionResource.php
+++ app/Filament/Resources/HospitalAdmissionResource.php
@@ -118,17 +118,17 @@ class HospitalAdmissionResource extends Resource
             ->paginationPageOptions([5, 10, 15, 20, 50, 100])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Html2MediaAction::make('print_admitting_order')
-                    ->icon('heroicon-o-printer')
-                    ->color('success')
-                    ->label('Admitting Order')
-                    ->content(function($record): View {
-                        return view('consultations.admitting-order', [
-                            'header_image' => $record->clinic->header_image,
-                            'patient' => $record->patient,
-                            'hospital' => $record->hospital
-                        ]);
-                    })
+                // Html2MediaAction::make('print_admitting_order')
+                //     ->icon('heroicon-o-printer')
+                //     ->color('success')
+                //     ->label('Admitting Order')
+                //     ->content(function($record): View {
+                //         return view('consultations.admitting-order', [
+                //             'header_image' => $record->clinic->header_image,
+                //             'patient' => $record->patient,
+                //             'hospital' => $record->hospital
+                //         ]);
+                //     })
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
