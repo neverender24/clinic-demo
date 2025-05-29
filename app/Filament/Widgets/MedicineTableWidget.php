a0fe21a7421da141467f95bcc7bed782c4dc16d0
George <neverender24@gmail.com>
2025-05-29 20:54:15 +0800

    widget

---
+++ app/Filament/Widgets/MedicineTableWidget.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Tables;
+use App\Models\Medicine;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Widgets\TableWidget as BaseWidget;
+use BezhanSalleh\FilamentShield\Traits\HasWidgetShield;
+use Dom\Text;
+
+class MedicineTableWidget extends BaseWidget
+{
+
+    use HasWidgetShield;
+    protected static ?int $sort = 4;
+protected int | string | array $columnSpan = 'full';
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                Medicine::take(10)
+            )
+            ->columns([
+                TextColumn::make("name")->searchable(),
+                TextColumn::make("consultations_count")->counts('consultations')->sortable(),
+            ])->defaultSort('consultations_count', 'desc');
+    }
+}
