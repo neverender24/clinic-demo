1cff38e22eb5edc263f8b3c81a198d797fcee478
javarex <raymartnayreitanong11@gmail.com>
2025-06-14 12:30:56 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Filament\Pages\Auth\Login;
-use App\Http\Controllers\ConsultationController;
 use Illuminate\Support\Facades\Route;
+use Spatie\Activitylog\Models\Activity;
+use App\Http\Controllers\ConsultationController;
 
 Route::get('/', Login::class);
 
 Route::get('print-prescription/{id}', [ConsultationController::class, 'print']);
+
